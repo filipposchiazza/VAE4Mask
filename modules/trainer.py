332b0e90c0c14227355875274a578fde69e7b8b2
filippo <filipposchiazza97@gmail.com>
2024-09-25 14:48:45 +0200

    Update with Dice coefficient

--- modules/trainer.py
+++ modules/trainer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
-from utils import checkpoint, swa_update
+from utils import checkpoint, swa_update, dice_coefficient
 
 
 class VaeTrainer():
@@ -64,9 +64,11 @@ class VaeTrainer():
         history = {'train_total_loss': [],
                    'train_reconstruction_loss': [],
                    'train_kl_divergence': [],
+                   'train_dice_coefficient': [],
                    'val_total_loss': [],
                    'val_reconstruction_loss': [],
-                   'val_kl_divergence': []}
+                   'val_kl_divergence': [],
+                   'val_dice_coefficient': []}
         
         for epoch in range(num_epochs):
 
@@ -83,18 +85,21 @@ class VaeTrainer():
             history['train_total_loss'].append(train_losses[0])
             history['train_reconstruction_loss'].append(train_losses[1])
             history['train_kl_divergence'].append(train_losses[2])
+            history['train_dice_coefficient'].append(train_losses[3])
 
             if val_dataloader is not None:
                 # Validation mode
                 self.model.eval()
 
                 # Evaluate the model
-                val_losses = self._validate(val_dataloader)
+                val_losses = self._validate(val_dataloader,
+                                            kl_weight=kl_weight)
 
                 # Update history
                 history['val_total_loss'].append(val_losses[0])
                 history['val_reconstruction_loss'].append(val_losses[1])
                 history['val_kl_divergence'].append(val_losses[2])
+                history['val_dice_coefficient'].append(val_losses[3])
 
             # Checkpoint
             checkpoint(model=self.model,
@@ -122,16 +127,18 @@ class VaeTrainer():
         running_total_loss = 0.0
         running_rec_loss = 0.0
         running_kl_loss = 0.0
+        running_dice = 0.0
 
         mean_total_loss = 0.0
         mean_rec_loss = 0.0
         mean_kl_loss = 0.0
+        mean_dice = 0.0
 
         self.optimizer.zero_grad()
 
         with tqdm(train_dataloader, unit='batches') as tepoch:
 
-            for batch_idx, (masks, ) in enumerate(tepoch):
+            for batch_idx, (masks, _) in enumerate(tepoch):
 
                 # Update the progress bar description
                 tepoch.set_description(f'Epoch {epoch+1}')
@@ -143,9 +150,12 @@ class VaeTrainer():
                 x_pred, mean, log_var = self.model(masks)
 
                 # Compute the losses
-                rec_loss = F.binary_cross_entropy(x_pred, masks, reduction='sum')
+                rec_loss = F.binary_cross_entropy(x_pred, masks, reduction='mean')
                 kl_loss = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
                 total_loss = rec_loss + kl_weight * kl_loss
+                with torch.no_grad():
+                    binary_pred = (x_pred > 0.5).float()
+                    dice = dice_coefficient(binary_pred, masks)
 
                 # Backward pass
                 total_loss.backward()
@@ -162,17 +172,20 @@ class VaeTrainer():
                 running_total_loss += total_loss.item()
                 running_rec_loss += rec_loss.item()
                 running_kl_loss += kl_loss.item()
+                running_dice += dice.item()
 
                 mean_total_loss = running_total_loss / (batch_idx + 1)
                 mean_rec_loss = running_rec_loss / (batch_idx + 1)
                 mean_kl_loss = running_kl_loss / (batch_idx + 1)
+                mean_dice = running_dice / (batch_idx + 1)
 
                 # Update the progress bar
                 tepoch.set_postfix(total_loss="{:.6f}".format(mean_total_loss),
                                    rec_loss="{:.6f}".format(mean_rec_loss),
-                                   kl_loss="{:.6f}".format(mean_kl_loss))
+                                   kl_loss="{:.6f}".format(mean_kl_loss),
+                                   dice="{:.2f}".format(mean_dice))
         
-        return mean_total_loss, mean_rec_loss, mean_kl_loss
+        return mean_total_loss, mean_rec_loss, mean_kl_loss, mean_dice
     
 
     def _validate(self, 
@@ -182,6 +195,7 @@ class VaeTrainer():
         running_total_loss = 0.0
         running_rec_loss = 0.0
         running_kl_loss = 0.0
+        running_dice = 0.0
 
         with torch.no_grad():
             for (masks, _) in val_dataloader:
@@ -196,19 +210,24 @@ class VaeTrainer():
                 rec_loss = F.binary_cross_entropy(x_pred, masks, reduction='sum')
                 kl_loss = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
                 total_loss = rec_loss + kl_weight * kl_loss
+                binary_pred = (x_pred > 0.5).float()
+                dice = dice_coefficient(binary_pred, masks)
 
                 # Update the running losses
                 running_total_loss += total_loss.item()
                 running_rec_loss += rec_loss.item()
                 running_kl_loss += kl_loss.item()
+                running_dice += dice.item()
 
         mean_total_loss = running_total_loss / len(val_dataloader)
         mean_rec_loss = running_rec_loss / len(val_dataloader)
         mean_kl_loss = running_kl_loss / len(val_dataloader)
+        mean_dice = running_dice / len(val_dataloader)
 
         print(f'Validation total loss: {mean_total_loss:.6f}, '
               f'Reconstruction loss: {mean_rec_loss:.6f}, '
-              f'KL loss: {mean_kl_loss:.6f}')
+              f'KL loss: {mean_kl_loss:.6f}',
+              f'Dice Coefficient: {mean_dice:.2f}')
         
-        return mean_total_loss, mean_rec_loss, mean_kl_loss
+        return mean_total_loss, mean_rec_loss, mean_kl_loss, mean_dice
     
