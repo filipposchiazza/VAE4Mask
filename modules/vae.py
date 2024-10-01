f6c90290c5f87c9599a38ee97ba0bb2927037414
filippo <filipposchiazza97@gmail.com>
2024-10-01 12:21:08 +0200

    Update loading methods

--- modules/vae.py
+++ modules/vae.py
@@ -55,19 +55,7 @@ class VAE(nn.Module):
                                base_channels=base_channels,
                                channel_multipliers=channel_multipliers,
                                num_res_blocks=num_res_blocks)
-        """
-        self.latent_conv = nn.Conv2d(in_channels=base_channels * channel_multipliers[-1],
-                                     out_channels=4,
-                                     kernel_size=1,
-                                     stride=1,
-                                     padding=0)
-        
-        self.decoder_input = nn.Conv2d(in_channels=2,
-                                       out_channels=base_channels * channel_multipliers[-1],
-                                       kernel_size=1,
-                                       stride=1,
-                                       padding=0)
-        """
+
         # Calculate the number of features in the linear layer
         self.shape_before_flatten = (base_channels * channel_multipliers[-1], 
                                      img_size // 2 ** len(channel_multipliers), 
@@ -262,57 +250,7 @@ class VAE(nn.Module):
         with open(history_file, 'rb') as f:
             history = pickle.load(f)
         return history
-    
 
-
-
-
-
-
-
-class AE(nn.Module):
-
-    def __init__(self, device):
-
-        super(AE, self).__init__()
-
-        self.l1 = nn.Linear(256*256, 2048)
-        self.l2 = nn.Linear(2048, 1024)
-        self.l3 = nn.Linear(1024, 512)
-        self.l4 = nn.Linear(512, 256)
-
-        self.l5 = nn.Linear(256, 512)
-        self.l6 = nn.Linear(512, 1024)
-        self.l7 = nn.Linear(1024, 2048)
-        self.l8 = nn.Linear(2048, 256*256)
-
-        self.num_parameters = self._calculate_num_parameters()
-        self.device = device
-
-    
-    def forward(self, x):
-        x = self.l1(x)
-        x = self.l2(x)
-        x = self.l3(x)
-        x = self.l4(x)
-        x = self.l5(x)
-        x = self.l6(x)
-        x = self.l7(x)
-        x = self.l8(x)
-
-        return nn.Sigmoid()(x)
-
-
-    def _calculate_num_parameters(self):
-        """Calculate the number of model parameters."""
-        num_parameters = 0
-        for param in self.parameters():
-            num_parameters += param.numel()
-        return num_parameters
-    
-
-
-        
 
         
     
