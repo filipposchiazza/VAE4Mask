de057f492193710a8af801e629216f0e681b331c
filippo <filipposchiazza97@gmail.com>
2024-10-01 12:24:23 +0200

    Add sigmoid activation to Decoder

--- modules/building_modules.py
+++ modules/building_modules.py
@@ -229,6 +229,7 @@ class Decoder(nn.Module):
                                                stride=2,
                                                padding=0)
             
+            self.sigmoid = nn.Sigmoid()
     
         def forward(self, x):
             
@@ -236,6 +237,7 @@ class Decoder(nn.Module):
                 x = layer(x)
             
             x = self.conv_out(x)
+            x = self.sigmoid(x)
             
             return x
     
